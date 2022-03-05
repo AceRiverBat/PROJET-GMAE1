5488d337190a90d2dc726875a5a6395375bf7ee9
Sarah <92088786+AceRiverBat@users.noreply.github.com>
2022-03-05 17:37:39 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let connexion = document.getElementById("bloc1");
+let firstco = document.getElementById("bloc2");
+let log = document.getElementById("log");
+
+log.addEventListener("click", function(){
+        connexion.style.display = "none";
+        firstco.style.display = "block";
+});
